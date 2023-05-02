2389e645d174625ac83c559288f5e07e80670fd9
ajh416 <henryada@oregonstate.edu>
2023-05-02 14:35:10 -0700

    remove errant nvim folder

--- nvim/after/plugin/everforest.lua
+++
@@ -1,6 +0,0 @@
-require('everforest').setup({
-	background = "hard",
-	transparent_background_level = 0,
-	italics = false,
-	disable_italic_comments = false,
-})
--- nvim/init.lua
+++
@@ -1,7 +0,0 @@
-vim.cmd[[set termguicolors]]
-vim.cmd[[set number]]
-vim.cmd[[colorscheme everforest]]
-
--- IMPORTS (lua/)
-require("plugins")
-require("remap")
